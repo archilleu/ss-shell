86539bbae420d54595b618e36a057d4c92837642
lrabbitmm <archilleu@users.noreply.github.com>
2019-11-25 11:03:26 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt -y --force-yes install git
 sudo apt -y --force-yes install shadowsocks-libev
+sudo wget https://raw.githubusercontent.com/archilleu/ss-shell/master/config.json
 sudo cp ./config.json /etc/shadowsocks-libev/config.json
 sudo /etc/init.d/shadowsocks-libev restart
